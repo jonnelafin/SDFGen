c3f20a3e806076ba1877b1c6e468e0d59ff01ab3
Elias Eskelinen <elias.eskelinen@pm.me>
2021-01-17 16:33:10 +0200

    Release .5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // <Readme>  //
-//! SDFGen generates a preferably lower resolution distance field of an input image.
+//! SDFGen generates a preferably lower resolution distance field from an input image.
 //!
 //! This distance field can be upscaled fast with bilinear filters built into gpus nowadays.
 //! Final sharp output of this upscale can be extracted using a threshold filter. With the default parameters in this program, threshold should be set to about 99%. 
@@ -11,7 +11,7 @@
 //! Without the release flag, the execution time can be up to 10 times slower.
 //! # Running the project
 //! ```console
-//!   ./target/release/sdf [OPTIONS]
+//!   ./target/release/sdfgen [OPTIONS]
 //! ```
 //! ```
 //! Optional arguments:
@@ -25,6 +25,8 @@
 //!   -t,--threads THREADS  Number of assigned threads. Defaults to 8.
 //!   -V,--version          Show version
 //! ```
+//! <!-- Please do not edit the README.md file directly, as it is compiled from the documentation at src/main.rs -->
+//! <!-- Instead, make the edits into the main.rs file and then run the compile_readme.sh or equivalent. --> 
 // </Readme> //
 
 //Main image parsing and manipulation
