de239ee315953731d469dac1e9d6037b0725225d
Elias Eskelinen <elias.eskelinen@pm.me>
2021-01-17 16:25:16 +0200

    readme updates

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@
 //! This distance field can be upscaled fast with bilinear filters built into gpus nowadays.
 //! Final sharp output of this upscale can be extracted using a threshold filter. With the default parameters in this program, threshold should be set to about 99%. 
 //!
-//! # Examples
-//! Run the project
-//! ```
-//! cargo run --release
+//! # Building the project
+//! ```console
+//! cargo build --release
 //! ```
 //! Without the release flag, the execution time can be up to 10 times slower.
-//!
-//! Currently all variables are hardcoded, they can be changed in the main program file "/scr/main.rs".
+//! # Running the project
+//! ```console
+//! Usage:
+//!   ./target/release/sdf [OPTIONS]
+//! Optional arguments:
+//!   -h,--help             Show this help message and exit
+//!   -v,--verbose          Be verbose
+//!   -f,--file FILE        filepath of the input image. Defaults to
+//!                         "images/sdf500.png"
+//!   -s,--scale SCALE      Scale of the distance field. For example resolution
+//!                         500x500 becomes 250x250 with scale 2 and 125x125 with
+//!                         scale 4. Defaults to 32.
+//!   -t,--threads THREADS  Number of assigned threads. Defaults to 8.
+//!   -V,--version          Show version
+//! ```
 // </Readme> //
 
 //Main image parsing and manipulation
