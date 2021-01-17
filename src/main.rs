d198116eac2136de87dd58e94ad6df77cca358a3
Elias Eskelinen <elias.eskelinen@pm.me>
2021-01-17 12:32:11 +0200

    further readme

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! SDFGen generates a preferably lower resolution distance field of an input image.
 //!
 //! This distance field can be upscaled fast with bilinear filters built into gpus nowadays.
-//! Final sharp output of this upscale can be extracted using a threshold filter, with the default parameters in this program, threshold should be set to about 99%. 
+//! Final sharp output of this upscale can be extracted using a threshold filter. With the default parameters in this program, threshold should be set to about 99%. 
 //!
 //! # Examples
 //! Run the project
