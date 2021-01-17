29935e9e193772d3ef5263d74b8f24da8db3b9b0
Elias Eskelinen <elias.eskelinen@pm.me>
2021-01-17 16:19:43 +0200

    arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// <Readme>  //
 //! SDFGen generates a preferably lower resolution distance field of an input image.
 //!
 //! This distance field can be upscaled fast with bilinear filters built into gpus nowadays.
@@ -11,23 +12,31 @@
 //! Without the release flag, the execution time can be up to 10 times slower.
 //!
 //! Currently all variables are hardcoded, they can be changed in the main program file "/scr/main.rs".
-extern crate chrono;
-use chrono::Utc;
+// </Readme> //
 
+//Main image parsing and manipulation
 extern crate image;
 use image::GenericImageView;
 use image::GenericImage;
 
+//Threading
 use std::thread;
 
+//Excecution time
+extern crate chrono;
+use chrono::Utc;
+
+//Argument parsing
+extern crate argparse;
+use argparse::{ArgumentParser, StoreTrue, Store};
 
 
 const WHITE: image::Rgba<u8> = image::Rgba([255u8, 255u8, 255u8, 255u8]);
 const INF: u32 = 9999999;
 //const MAX_DIST: u32 = 32;
-const SCALE: u32 = 32;
+//const scale: u32 = 32;
 
-const THREADS: usize = 8;
+//const threads: usize = 16;
 
 fn get_active(img: &image::DynamicImage) -> Vec<[u32;2]>{
 	let w = img.dimensions().0;
@@ -51,9 +60,12 @@ fn get_active(img: &image::DynamicImage) -> Vec<[u32;2]>{
     		}
     		ind += 1;
 		}
-		print!("[{}%]: {}/{}", ((ind as f64)/(full as f64) * 100.0) as u32, ind, full);
-		println!("");
+		let percent = (ind as f64)/(full as f64) * 100.0;
+		if percent % 10.0 == 0.0{
+			print!("[{}%]: {}/{} \r", percent as u32, ind, full);
+		}
 	}
+	println!("");
 	out
 }
 
@@ -94,10 +106,10 @@ fn get_closest(active: &Vec<[u32; 2]>, value: &[u32; 2], second_pass: bool) -> [
 	return [(best, bests), (best, bests), (best, bests)];
 }
 
-fn gen_sdf(img: &image::DynamicImage, active: &Vec<[u32; 2]>, from: [u32; 2], to: [u32; 2], id: u32) -> image::DynamicImage{
-//	let SCALE = 4;
-	let w = img.dimensions().0/SCALE;
-	let h = img.dimensions().1/SCALE;
+fn gen_sdf(img: &image::DynamicImage, active: &Vec<[u32; 2]>, from: [u32; 2], to: [u32; 2], id: u32, scale: u32) -> image::DynamicImage{
+//	let scale = 4;
+	let w = img.dimensions().0/scale;
+	let h = img.dimensions().1/scale;
 	let _ws = w as usize;
 	let _hs = h as usize;
 	
@@ -117,20 +129,20 @@ fn gen_sdf(img: &image::DynamicImage, active: &Vec<[u32; 2]>, from: [u32; 2], to
 		let _xs = x as usize;
 		for y in from[1]..to_y{
 			let _ys = y as usize;
-			let closest = get_closest(&active, &[x*SCALE, y*SCALE], false);
+			let closest = get_closest(&active, &[x*scale, y*scale], false);
 			let r = 255u8 - (closest[0].1 as f64 / max_dist) as u8;
 			let g = 255u8 - (closest[1].1 as f64 / max_dist) as u8;
 			let b = 255u8 - (closest[2].1 as f64 / max_dist) as u8;
     		out.put_pixel(y, x, image::Rgba([r,g,b, 255]));
-    		//img.put_pixel(y*SCALE, x*SCALE, image::Rgba([r,r,r, 255])); //nice effect
+    		//img.put_pixel(y*scale, x*scale, image::Rgba([r,r,r, 255])); //nice effect
     		ind += 1;
 		}
 		let percent = ((ind as f64)/(full as f64) * 100.0) as u32;
 		if percent % 10 == 0{
-			print!("{}[{}%]: {}/{}", id, percent, ind, full);
-			println!("");
+			print!("{}[{}%]: {}/{} \r", id, percent, ind, full);
 		}
 	}
+	println!("");
 	out
 }
 fn create_shadow_copy(img: &image::DynamicImage) -> image::DynamicImage{
@@ -162,11 +174,35 @@ fn join_images(img: &image::DynamicImage, img2: &image::DynamicImage) -> image::
 }
 
 fn main() {
+    let mut verbose = false;
+
+    let mut filename = "images/sdf500.png".to_string();
+    let mut threads = 4;
+    let mut scale = 32;
+	{  // this block limits scope of borrows by ap.refer() method
+		let mut ap = ArgumentParser::new();
+		ap.set_description("Generates a distance field from any given image.");
+		ap.refer(&mut verbose)
+			.add_option(&["-v", "--verbose"], StoreTrue,
+			"Be verbose");
+		ap.refer(&mut filename)
+			.add_option(&["-f", "--file"], Store,
+			"filepath of the input image. Defaults to \"images/sdf500.png\"");
+		ap.refer(&mut scale)
+			.add_option(&["-s" ,"--scale"], Store,
+			"Scale of the distance field. For example resolution 500x500 becomes 250x250 with scale 2 and 125x125 with scale 4. Defaults to 32.");
+		ap.refer(&mut threads)
+			.add_option(&["-t" ,"--threads"], Store,
+			"Number of assigned threads. Defaults to 8.");
+		ap.add_option(&["-V", "--version"],
+		    argparse::Print(env!("CARGO_PKG_VERSION").to_string()), "Show version");
+		ap.parse_args_or_exit();
+	}
     let start_time = Utc::now().time();
     println!("Opening image...");
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
-    let img = image::open("images/smiley.png").unwrap();
+    let img = image::open(filename).unwrap();
 
     // The dimensions method returns the images width and height.
     println!("dimensions {:?}", img.dimensions());
@@ -180,23 +216,23 @@ fn main() {
 	let mut img3 = create_shadow_copy(&img);
     
     let mut handles= vec![];
-    for i in 0..THREADS{
+    for i in 0..threads{
     	let i2 = i as u32;
 	    let img_clone = img.clone();
 	    let active_clone = active.clone();
 	    let handle = thread::spawn(move || {
-			let w = img_clone.dimensions().0/SCALE;
-			let _h = img_clone.dimensions().1/SCALE;
-			gen_sdf(&img_clone, &active_clone, [0,((w/THREADS as u32)*(i2))], [0,((w/THREADS as u32)*(i2+1))], i2)
+			let w = img_clone.dimensions().0/scale;
+			let _h = img_clone.dimensions().1/scale;
+			gen_sdf(&img_clone, &active_clone, [0,((w/threads as u32)*(i2))], [0,((w/threads as u32)*(i2+1))], i2, scale)
 		});
 		handles.push(handle);
 //	    let handle2 = thread::spawn(move || {
-//			let w = img_clone.dimensions().0/SCALE;
-//			let h = img_clone.dimensions().1/SCALE;
+//			let w = img_clone.dimensions().0/scale;
+//			let h = img_clone.dimensions().1/scale;
 //			gen_sdf(&img_clone, &active_clone, [0,w/2], [0,0])
 //		});
 	}
-	for _ in 0..THREADS{
+	for _ in 0..threads{
 		let result = handles.pop();
 //		let backup = img3.clone();
 		img3 = match result{
@@ -207,8 +243,8 @@ fn main() {
 		
 	}
 	println!("Cropping...");
-	let w = img3.dimensions().0/SCALE;
-	let h = img3.dimensions().1/SCALE;
+	let w = img3.dimensions().0/scale;
+	let h = img3.dimensions().1/scale;
 	let fin = image::DynamicImage::crop(&mut img3, 0, 0, w, h);
     println!("Saving pixels...");
     // Write the contents of this image to the Writer in PNG format.
